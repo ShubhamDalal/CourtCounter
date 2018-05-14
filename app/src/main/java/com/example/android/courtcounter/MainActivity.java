2be00d170a602991ba8ebd54f58cd9b9f5f772a9
ShubhamDalal <srkdalal@gmail.com>
2018-05-14 20:56:51 +0530

    Changed DataType Char to Enum to DisplayAction via SwitchCase.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -14,55 +14,68 @@ int bTeamScore= 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+// Using Enums to Display Action via Switch
+    public enum ActionDisplay {
+        TEAM_A,
+        TEAM_B,
+        RESET
+    }
+
+    ActionDisplay teamA = ActionDisplay.TEAM_A;
+    ActionDisplay teamB = ActionDisplay.TEAM_B;
+    ActionDisplay reset = ActionDisplay.RESET;
+
 // These Methods are for Team A
     public void add3ToTeamA(View view){
         aTeamScore+=3;
-        display('A');
+        display(teamA);
     }
     public void add2ToTeamA(View view){
         aTeamScore+=2;
-        display('A');
+        display(teamA);
     }
     public void freeToTeamA(View view){
         aTeamScore+=1;
-        display('A');
+        display(teamA);
     }
 
 // These Methods are for Team B
     public void add3ToTeamB(View view){
         bTeamScore+=3;
-        display('B');
+        display(teamB);
     }
     public void add2ToTeamB(View view){
         bTeamScore+=2;
-        display('B');
+        display(teamB);
     }
     public void freeToTeamB(View view){
         bTeamScore+=1;
-        display('B');
+        display(teamB);
     }
 
 //    This Method Resets Score of both the teams.
     public void resetTeams(View view){
         aTeamScore =0;
         bTeamScore =0;
-        display('R');
+        display(reset);
     }
 
 // These Method displays the Score
-    private void display(char team) {
+    private void display(ActionDisplay Action) {
         TextView aTeamTextView = (TextView) findViewById(R.id.aTeam);
         TextView bTeamTextView = (TextView) findViewById(R.id.bTeam);
-        switch(team){
-            case 'A':
+
+        switch(Action){
+            case TEAM_A:
                 aTeamTextView.setText("" + aTeamScore);
                 break;
 
-            case 'B':
+            case TEAM_B:
                 bTeamTextView.setText("" + bTeamScore);
                 break;
 
-            case 'R':
+            case RESET:
                 aTeamTextView.setText(""+aTeamScore);
                 bTeamTextView.setText(""+bTeamScore);
                 break;
